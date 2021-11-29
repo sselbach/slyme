3831ffe538ae2ba7a87af53ef24f584c5ddd13f7
Sören Selbach <sselbach@uos.de>
2021-11-29 23:19:44 +0100

    agents now bounce of walls

--- main.py
+++ main.py
@@ -3,8 +3,22 @@ import moderngl as mgl
 import moderngl_window as mglw
 from moderngl_window import geometry
 
+import numpy as np
+
+from array import array
+from random import random
+from math import sin, cos, pi
+
 
 WIDTH, HEIGHT = 1920, 1080
+N_AGENTS = 100
+
+
+def uniform_direction():
+    angle = random() * 2 * pi
+
+    return cos(angle), sin(angle)
+
 
 class ComputeRenderToTexture(mglw.WindowConfig):
     title = "He's Moldin'"
@@ -21,6 +35,7 @@ class ComputeRenderToTexture(mglw.WindowConfig):
         try:
             self.compute_shader["width"] = WIDTH
             self.compute_shader["height"] = HEIGHT
+            self.compute_shader["speed"] = 2
         
         except KeyError as e:
             print("Warning: Tried setting a uniform that does not exist or is not active within the shader.")
@@ -32,30 +47,40 @@ class ComputeRenderToTexture(mglw.WindowConfig):
         # default dtype of textures is 'f1', i.e. uint8 normalized to the range between 0 and 1
         self.texture = self.ctx.texture((WIDTH, HEIGHT), 4)
         self.texture.filter = mgl.LINEAR, mgl.LINEAR
+
+        # Agent: vec2 pos, vec2 dir
+        self.buffer_agent = self.ctx.buffer(array("f", self._gen_initial_data(N_AGENTS)))
+
         
     def render(self, time, frame_time):
         self.ctx.clear(0.3, 0.3, 0.3)
 
-        w, h = self.texture.size
-        gw, gh = 16, 16
-        nx, ny, nz = int(w / gw), int(h / gh), 1
-
-
-        nx, ny, nz = 10, 1, 1
-
-        try:
-            self.compute_shader["time"] = time
-
-        except KeyError as e:
-            print(e)
+        nx, ny, nz = N_AGENTS // 512 + 1, 1, 1
 
         # Automatically binds as a GL_R32F / r32f (read from the texture)
-        self.texture.bind_to_image(0, read=False, write=True)
+        self.texture.bind_to_image(0, read=True, write=True)
+
+        # bind Agent buffer to the shader
+        self.buffer_agent.bind_to_storage_buffer(0)
+
         self.compute_shader.run(nx, ny, nz)
 
         # Render texture
         self.texture.use(location=0)
         self.quad_fs.render(self.texture_program)
+
+    def _gen_initial_data(self, n_agents):
+        for _ in range(n_agents):
+            # position
+            yield WIDTH / 2
+            yield HEIGHT / 2
+
+            # direction
+            x, y = uniform_direction()
+            individual_speed = random()
+
+            yield x * individual_speed
+            yield y * individual_speed
 
 
 if __name__ == "__main__":
--- resources/slime_mold.glsl
+++ resources/slime_mold.glsl
@@ -5,57 +5,71 @@
 
 // in this shader, we are not separating the work based on image segments, but instead based on agents
 // therefore, we only use one dimension of local_size
-layout (local_size_x = 16, local_size_y = 1, local_size_z = 1) in;
+layout (local_size_x = 512, local_size_y = 1, local_size_z = 1) in;
 
-// match the input texture format!
+// the trail map is an image bound at location 0
 layout (rgba8, location = 0) uniform image2D trailMap;
-
-uniform float time;
-uniform uint width;
-uniform uint height;
 
 struct Agent {
     vec2 pos;
     vec2 dir;
 };
 
-layout (std430, binding = 0) buffer buf_ants {
+layout (std140, binding = 0) buffer buf_ants {
     Agent agents[];
 } Buf_agents;
 
+uniform uint width;
+uniform uint height;
 uniform uint n_agents;
+uniform float speed;
+uniform float steerStrength;
+uniform float sensorAngleSpacing;
+uniform int sensorSize;
+uniform int sensorDistance;
 
-// hash function for psuedo random number generation
+// time is used for pseudo-random number generation
+uniform uint timer;
+
+
 uint hash(uint state) {
-    state ^= 2747636419;
-    state *= 2654435769;
-    state ^= state >> 16;
-    state *= 2654435769;
-    state ^= state >> 16;
-    state *= 2654435769;
+    state ^= 2747636419u;
+    state *= 2654435769u;
+    state ^= state >> 16u;
+    state *= 2654435769u;
+    state ^= state >> 16u;
+    state *= 2654435769u;
     return state;
 }
 
+const float maxPossibleHash = 4294967295.0;
+
 void main() {
-    // texel coordinate we are writing to
-    ivec2 texelPos = ivec2(gl_GlobalInvocationID.xy);
+    // agent index is the global invocation id
+    const uint id = gl_GlobalInvocationID.x;
 
-    // convert to 1D index
-    uint pixelIndex = texelPos.y * width + texelPos.x;
-    uint pseudoRandomNumber = hash(pixelIndex * int(time * 144));
+    Agent agent = Buf_agents.agents[id];
 
-    float maxPossibleHash = 4294967295.0;
-    float normalized = pseudoRandomNumber / maxPossibleHash;
+    vec2 newPos = agent.pos + agent.dir * speed;
+    vec2 newDir = agent.dir;
 
+    // check if agent is out of map and adjust direction
+    if (newPos.x < 0 || newPos.x > width) {
+        newPos.x = clamp(newPos.x, 0.1, width - 0.1);
+        newDir.x *= -1;
+    }
 
-    imageStore(
-        trailMap,
-        texelPos,
-        vec4(
-            normalized,
-            normalized,
-            normalized,
-            1.0
-        )
-    );
+    if (newPos.y < 0 || newPos.y > height) {
+        newPos.y = clamp(newPos.y, 0.1, height - 0.1);
+        newDir.y *= -1;
+    }
+
+    // update Agent buffer with the new position and direction
+    Buf_agents.agents[id].pos = newPos;
+    Buf_agents.agents[id].dir = newDir;
+
+    ivec2 texelPos = ivec2(agent.pos);
+    const vec4 texel = vec4(1.0);
+    
+    imageStore(trailMap, texelPos, texel);
 }
